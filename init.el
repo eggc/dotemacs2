ed092331812f876e654883c67b862c14741cb3c3
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2021-04-18 23:34:31 +0900

    go の設定を追加

--- init.el
+++ init.el
@@ -316,7 +316,11 @@
 
 (use-package go-mode
   :config
-  (add-to-list 'company-backends 'company-go))
+  (add-to-list 'company-backends 'company-go)
+  :init
+  (defun go-mode-custom-hook () (setq-local tab-width 4))
+  :hook
+  (go-mode . go-mode-custom-hook))
 
 (use-package slim-mode)
 (use-package string-inflection
