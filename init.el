29320ffdd6555e617f85da1e061faa0e86f5fc17
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2021-05-30 11:36:55 +0900

    ts, tsx は typescript-mode で起動する

--- init.el
+++ init.el
@@ -270,7 +270,7 @@
   (add-hook 'web-mode-hook (lambda()
     (setq web-mode-code-indent-offset 2)
     (setq web-mode-markup-indent-offset 2)))
-  :mode ("\\.js[x]?$" "\\.ts[x]?$"))
+  :mode ("\\.js[x]?$"))
 
 (use-package eww
   :config
