790292005ab34677f9644de056ff8aa82ef9b7ea
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2021-12-01 17:11:14 +0900

    Add settings for eglot in order to use typescript langage server

--- lib/lsp.el
+++ lib/lsp.el
@@ -1,1 +1,7 @@
-(use-package eglot)
+(use-package eglot
+  :config
+  (add-to-list 'eglot-server-programs '(typescript-mode . ("/Users/eguchi.ken/.nvm/versions/node/v12.19.0/bin/typescript-language-server" "--stdio")))
+  (add-to-list 'eglot-server-programs '(tsx-mode . ("/Users/eguchi.ken/.nvm/versions/node/v12.19.0/bin/typescript-language-server" "--stdio")))
+  :hook
+  (typescript-mode . eglot-ensure)
+  (tsx-mode . eglot-ensure))
