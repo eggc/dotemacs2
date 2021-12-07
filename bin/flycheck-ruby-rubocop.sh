a5621b9bdc30e872b4254969836433057c3d3cce
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2021-12-07 15:40:30 +0900

    Use rubocop-daemon-wrapper on flycheck

--- bin/flycheck-ruby-rubocop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if bundle exec gem list --installed rubocop &> /dev/null; then
-    bundle exec rubocop "$@"
-elif gem list --installed rubocop > /dev/null; then
-    rubocop "$@"
-fi
--- lib/flycheck.el
+++ lib/flycheck.el
@@ -2,4 +2,4 @@
   :hook
   (ruby-mode . flycheck-mode)
   :config
-  (custom-set-variables '(flycheck-ruby-rubocop-executable (concat dotemacs2-directory "bin/flycheck-ruby-rubocop.sh"))))
+  (custom-set-variables '(flycheck-ruby-rubocop-executable "rubocop-daemon-wrapper")))
