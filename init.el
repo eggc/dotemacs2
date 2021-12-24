61a223721e9d9c067778133e6eb6bef9f816a199
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2021-12-25 01:41:29 +0900

    Remove outline-mode settings which cause wrong itemization

--- init.el
+++ init.el
@@ -131,17 +131,7 @@
 (use-package yaml-mode
   :mode (".yaml$")
   :hook
-  (yaml-mode . yaml-mode-outline-hook)
-  :init
-  (defun yaml-outline-level ()
-    "Return the outline level based on the indentation, hardcoded at 2 spaces."
-    (s-count-matches "[ ]\\{2\\}" (match-string 0)))
-  (defun yaml-mode-outline-hook ()
-    (outline-minor-mode)
-    (setq outline-regexp "^\\([ ]\\{2\\}\\)*\\([-] \\)?\\([\"][^\"]*[\"]\\|[a-zA-Z0-9_-]*\\): *\\([>|]\\|&[a-zA-Z0-9_-]*\\)?$")
-    (setq outline-level 'yaml-outline-level))
-  :config
-  (define-key yaml-mode-map (kbd "TAB") 'outline-toggle-children))
+  (yaml-mode . yaml-mode-outline-hook))
 
 (use-package go-mode
   :config
