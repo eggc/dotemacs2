6a789fda694136f7f9544fa2b8145a7450a6d01a
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2022-01-28 18:32:45 +0900

    forge は git の普段遣いを遅くするため利用しない

--- lib/git.el
+++ lib/git.el
@@ -5,12 +5,12 @@
   (remove-hook 'server-switch-hook 'magit-commit-diff)
   :chords (("gh" . magit-status)))
 
-(use-package forge
-  :after magit
-  :config
-  ;; (remove-hook 'magit-status-sections-hook 'forge-insert-pullreqs)
-  ;; (remove-hook 'magit-status-sections-hook 'forge-insert-issues)
-  (defvar forge-topic-list-columns
-        '(("#" 5 forge-topic-list-sort-by-number (:right-align t) number nil)
-          ("Assignees" 15 t nil assignees nil)
-          ("Title" 35 t nil title  nil))))
+;; (use-package forge
+;;   :after magit
+;;   :config
+;;   ;; (remove-hook 'magit-status-sections-hook 'forge-insert-pullreqs)
+;;   ;; (remove-hook 'magit-status-sections-hook 'forge-insert-issues)
+;;   (defvar forge-topic-list-columns
+;;         '(("#" 5 forge-topic-list-sort-by-number (:right-align t) number nil)
+;;           ("Assignees" 15 t nil assignees nil)
+;;           ("Title" 35 t nil title  nil))))
