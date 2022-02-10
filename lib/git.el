278c92adf793f0a2b420b2adddcc0f84bc66afa3
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2022-02-10 11:09:24 +0900

    magit status をより速くする

--- lib/git.el
+++ lib/git.el
@@ -1,6 +1,10 @@
 (use-package magit
   :config
   (defvar magit-diff-refine-hunk t)
+  ; magit をとにかく軽量にしたいので普段見ない情報は計算しない
+  (remove-hook 'magit-status-sections-hook 'magit-insert-status-headers)
+  (remove-hook 'magit-status-sections-hook 'magit-insert-unpushed-to-upstream-or-recent)
+  (remove-hook 'magit-status-sections-hook 'magit-insert-unpushed-to-pushremote)
   (remove-hook 'magit-refs-sections-hook 'magit-insert-tags)
   (remove-hook 'server-switch-hook 'magit-commit-diff)
   :bind (("s-g" . magit-status)))
