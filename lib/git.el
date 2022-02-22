b4029f22ec8ec7a8b713c1b03a21f312690c827d
eguchi.ken <eguchi.ken@moneyforward.co.jp>
2022-02-22 20:23:15 +0900

    advice-add を使う

--- lib/git.el
+++ lib/git.el
@@ -2,17 +2,17 @@
   :config
   (defvar magit-diff-refine-hunk t)
   ; magit をとにかく軽量にしたいので普段見ない情報は計算しない
-  (remove-hook 'magit-status-sections-hook 'magit-insert-status-headers)
-  (remove-hook 'magit-status-sections-hook 'magit-insert-unpushed-to-upstream-or-recent)
-  (remove-hook 'magit-status-sections-hook 'magit-insert-unpushed-to-pushremote)
-  (remove-hook 'magit-refs-sections-hook 'magit-insert-tags)
-  (remove-hook 'server-switch-hook 'magit-commit-diff)
+  (remove-hook #'magit-status-sections-hook #'magit-insert-status-headers)
+  (remove-hook #'magit-status-sections-hook #'magit-insert-unpushed-to-upstream-or-recent)
+  (remove-hook #'magit-status-sections-hook #'magit-insert-unpushed-to-pushremote)
+  (remove-hook #'magit-refs-sections-hook   #'magit-insert-tags)
+  (remove-hook #'server-switch-hook         #'magit-commit-diff)
   :bind (("s-g" . magit-status)))
 
 (use-package browse-at-remote
   :config
-  (defun browse-at-remote--get-local-branch () "master") ;; master ブランチを常に見る
-  (defun browse-at-remote--get-remotes () '("upstream")) ;; upstream リポジトリを常に見る
+  (advice-add #'browse-at-remote--get-local-branch :override (lambda () "master")) ;; master ブランチを常に見る
+  (advice-add #'browse-at-remote--get-remotes :override (lambda () '("upstream"))) ;; upstream リポジトリを常に見る
 )
 
 ;; (use-package forge
