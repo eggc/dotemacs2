bc8232428809917bf40dfe1e8705494771638ad5
eggc <no.eggchicken@gmail.com>
2022-12-01 11:12:50 +0900

    ♻️ refactor: Remove ripgrep; it no longer use

--- lib/eg-grep.el
+++ lib/eg-grep.el
@@ -10,14 +10,4 @@
   (setq grep-find-ignored-files (append grep-find-ignored-files my-ignore-files))
 )
 
-(use-package ripgrep
-  :if (file-exists-p "/usr/local/bin/rg")
-  :config
-  (defun ripgrep-regexp-with-arguments (regexp directory args)
-  (interactive
-    (list (read-from-minibuffer "Ripgrep search for: ")
-          (read-directory-name "Directory: ")
-          (read-from-minibuffer "optional arguments: ")))
-    (ripgrep-regexp regexp directory (list args))))
-
 (provide 'eg-grep)
