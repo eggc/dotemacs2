88f93409c63d3cf8c00d134fd7b342096858ff75
eggc <no.eggchicken@gmail.com>
2022-12-15 00:42:53 +0900

    ✨ feat: Customize `rspec-toggle-spec-and-target`

--- lib/eg-ruby.el
+++ lib/eg-ruby.el
@@ -24,11 +24,29 @@
   :bind (("C-c , R" . rubocop-autocorrect-current-file)
          ("C-c C-, C-r" . rubocop-autocorrect-current-file)))
 
+(defun rspec-specize-file-name-advice (args)
+  "controller からテストファイルを探索する時に request spec に移動するパッチ"
+  (let ((file-name (nth 0 args)))
+    (setq file-name (string-replace "/controllers/" "/requests/" file-name))
+    (setq file-name (string-replace "_controller.rb" ".rb" file-name))
+    (list file-name)
+    ))
+
+(defun rspec-targetize-file-name-advice (args)
+  "request spec からプロダクトコードを探索する時に controller に移動するパッチ"
+  (let ((file-name (nth 0 args)) (extension (nth 1 args)))
+    (setq file-name (string-replace "/requests/" "/controllers/" file-name))
+    (setq file-name (string-replace "_spec" "_controller" file-name))
+    (list file-name extension)
+    ))
+
 (use-package rspec-mode
   :config
   (setq rspec-use-spring-when-possible nil)
   (setq compilation-scroll-output "first-error")
   (defun rspec-runner () "bin/rspec")
+  (advice-add 'rspec-specize-file-name :filter-args 'rspec-specize-file-name-advice)
+  (advice-add 'rspec-targetize-file-name :filter-args 'rspec-targetize-file-name-advice)
   ;; @see https://github.com/pezra/rspec-mode#debugging
   :hook
   (after-init . inf-ruby-switch-setup)
