fd058267fffc24f6b68b376812ca6642913b8cdd
eggc <no.eggchicken@gmail.com>
2023-10-23 18:50:31 +0900

    🐛 fix: Use rg because git grep >= 2.39 is not working

--- lib/eg-jump.el
+++ lib/eg-jump.el
@@ -3,6 +3,7 @@
 (use-package dumb-jump
   :config
   (setq dumb-jump-max-find-time 5)
+  (setq dumb-jump-force-searcher 'rg)
   :init
   (add-hook 'xref-backend-functions #'dumb-jump-xref-activate))
 
