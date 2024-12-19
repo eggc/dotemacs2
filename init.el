914acddd4b82debc6fdd07a9380a1016d3c295dc
Ken Eguchi <ken.eguchi@litalico.co.jp>
2024-12-19 09:45:33 +0900

    ✨ feat: Add copilot settings such as key bindings

--- init.el
+++ init.el
@@ -57,4 +57,19 @@
 (use-package atomic-chrome :config (atomic-chrome-start-server))
 
 (when eg-enable-copilot
-  (use-package copilot :straight (:host github :repo "copilot-emacs/copilot.el" :files ("*.el"))))
+  (use-package copilot
+    :straight (:host github :repo "copilot-emacs/copilot.el" :files ("*.el"))
+    :hook (prog-mode . copilot-mode)
+    :bind (:map copilot-completion-map
+                ("<tab>" . 'copilot-accept-completion)
+                ("TAB" . 'copilot-accept-completion)
+                ("C-TAB" . 'copilot-accept-completion-by-word)
+                ("C-<tab>" . 'copilot-accept-completion-by-word)
+                ("C-n" . 'copilot-next-completion)
+                ("C-p" . 'copilot-previous-completion))
+    :config
+    (add-to-list 'copilot-indentation-alist '(prog-mode 2))
+    (add-to-list 'copilot-indentation-alist '(org-mode 2))
+    (add-to-list 'copilot-indentation-alist '(text-mode 2))
+    (add-to-list 'copilot-indentation-alist '(emacs-lisp-mode 2))
+    ))
